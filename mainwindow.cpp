9f5e2a2bebd5c1c5bce40fb84e492b93e5b451c6
mr.li <onground@163.com>
2025-05-13 19:01:20 +0800

    init ci

---
+++ mainwindow.cpp
@@ -0,0 +1,275 @@
+﻿#include <QMessageBox>
+#include <QRandomGenerator>
+#include <QNetworkDatagram>
+#include <QButtonGroup>
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include "common_tools/common_tool_func.h"
+#include "logger/logger.h"
+#include "config_recorder/uiconfigrecorder.h"
+
+#define RECV_DATA_NOTE_E(e) e
+
+#define RECV_DATA_NOTES \
+    RECV_DATA_NOTE_E(NORMAL), \
+    RECV_DATA_NOTE_E(START_ACK), \
+    RECV_DATA_NOTE_E(STOP_ACK), \
+    RECV_DATA_NOTE_E(UNEXPECTED_IN_START_WAIT), \
+    RECV_DATA_NOTE_E(UNEXPECTED_IN_STOP_WAIT), \
+    RECV_DATA_NOTE_E(IRRELEVANT_ADDR), \
+    RECV_DATA_NOTE_E(RECV_IN_IDLE),
+
+typedef enum
+{
+    RECV_DATA_NOTES
+}recv_data_notes_e_t;
+
+typedef struct
+{
+    recv_data_notes_e_t  notes;
+    QByteArray data;
+}recv_data_with_notes_s_t;
+
+static const char gs_start_req_cmd[] = {'\x00', '\x00', '\x00', '\x02'};
+static const char gs_start_ack_cmd[] = {'\x00', '\x00', '\x00', '\x02'};
+static const char gs_stop_req_cmd[] = {'\x00', '\x00', '\x00', '\x03'};
+static const char gs_stop_ack_cmd[] = {'\x00', '\x00', '\x00', '\x03'};
+
+static const quint16 gs_def_local_port = 8020;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow), m_cfg_recorder(this),
+      m_localport(gs_def_local_port), counter(0), collectingState(ST_IDLE)
+{
+    ui->setupUi(this);
+
+    m_start_req = QByteArray::fromRawData(gs_start_req_cmd, sizeof(gs_start_req_cmd));
+    m_start_ack = QByteArray::fromRawData(gs_start_ack_cmd, sizeof(gs_start_ack_cmd));
+    m_stop_req = QByteArray::fromRawData(gs_stop_req_cmd, sizeof(gs_stop_req_cmd));
+    m_stop_ack = QByteArray::fromRawData(gs_stop_ack_cmd, sizeof(gs_stop_ack_cmd));
+
+    m_cfg_filter_in.clear();
+    m_cfg_filter_out << ui->rmtIPLEdit << ui->rmtPortLEdit << ui->fileDescLEdit << ui->infoDispEdit;
+    /*set ui default cfg-----------------------------------------------------------*/
+    QButtonGroup *d_src_rbtn_grp = new QButtonGroup(this);
+    d_src_rbtn_grp->addButton(ui->randDataRBtn);
+    d_src_rbtn_grp->addButton(ui->fileDataRBtn);
+    ui->fileDataRBtn->setChecked(true);
+    ui->randDataRBtn->setChecked(true);
+
+    ui->infinDataCheckBox->setChecked(false);
+
+    /*load saved ui cfg-----------------------------------------------------------*/
+    m_cfg_recorder.load_configs_to_ui(this, m_cfg_filter_in, m_cfg_filter_out);
+
+    /*-----------------------------------------------------------*/
+    if (!udpSocket.bind(QHostAddress::AnyIPv4, m_localport))
+    {
+        QMessageBox::critical(this, "", QString("bind error"));
+        return;
+    }
+    connect(&udpSocket, &QUdpSocket::readyRead, this, &MainWindow::data_ready_hdlr);
+    connect(&m_send_timer, &QTimer::timeout, this, &MainWindow::send_int_timer_hdlr);
+}
+
+MainWindow::~MainWindow()
+{
+    m_cfg_recorder.record_ui_configs(this, m_cfg_filter_in, m_cfg_filter_out);
+    m_cfg_filter_in.clear();
+    m_cfg_filter_out.clear();
+
+    m_send_timer.stop();
+
+    delete ui;
+}
+
+void MainWindow::send_one_row()
+{
+    static const int ls_byte_per_point = 3;
+    int byteCount = ls_byte_per_point * ui->ptPerRowSpinBox->value();
+
+    switch(m_data_source_type)
+    {
+    case RAND_DATA_BYTES:
+    default:
+        QByteArray data = generateRandomData(byteCount);
+
+        // Append 2-byte counter (little-endian)
+        data.append(static_cast<char>(counter & 0xFF));
+        data.append(static_cast<char>((counter >> 8) & 0xFF));
+
+        // Increment counter for next send
+        counter++;
+
+        // Send data via UDP
+        qint64 bytesSent = udpSocket.writeDatagram(data, m_rmt_addr, m_rmt_port);
+        QString info_str = byteArrayToHexString(data);
+        ui->infoDispEdit->append(log_disp_prepender_str() + "sent:" + info_str);
+        if (bytesSent == -1)
+        {
+            info_str = QString("ERROR!!! Failed to send data: ") + udpSocket.errorString();
+            ui->infoDispEdit->append(info_str);
+        }
+
+        break;
+    }
+}
+
+void MainWindow::on_sendBtn_clicked()
+{
+    if (!validateInputs())
+    {
+        return;
+    }
+
+    m_rmt_addr = QHostAddress((ui->rmtIPLEdit->text()));
+    m_rmt_port = ui->rmtPortLEdit->text().toInt();
+
+    m_data_source_type = RAND_DATA_BYTES;
+    send_one_row();
+
+    if(counter < ui->rowsToSendSpinBox->value())
+    {
+        m_send_timer.start(ui->rowIntSpinBox->value());
+    }
+}
+
+void MainWindow::send_int_timer_hdlr()
+{
+    send_one_row();
+
+    if(counter >= ui->rowsToSendSpinBox->value())
+    {
+        m_send_timer.stop();
+    }
+}
+
+bool MainWindow::validateInputs()
+{
+    bool tr_ret;
+    // Validate IP address
+    if (ui->rmtIPLEdit->text().isEmpty())
+    {
+        QMessageBox::warning(this, "Validation Error", "Please enter a valid IP address.");
+        return false;
+    }
+    int port = ui->rmtPortLEdit->text().toInt(&tr_ret);
+    if(!tr_ret || port > 65535 || port <= 0)
+    {
+        QMessageBox::warning(this, "Validation Error", "invalid rmt port");
+        return false;
+    }
+
+    return true;
+}
+
+QByteArray MainWindow::generateRandomData(int byteCount)
+{
+    QByteArray data;
+    data.resize(byteCount);
+
+    // Fill the byte array with random values
+    QRandomGenerator *generator = QRandomGenerator::global();
+    for (int i = 0; i < byteCount; i++) {
+        data[i] = static_cast<char>(generator->bounded(256)); // 0-255
+    }
+
+    return data;
+}
+
+QString MainWindow::byteArrayToHexString(const QByteArray &data)
+{
+    QString result;
+
+    for (int i = 0; i < data.size(); i++) {
+        // Format each byte as a 2-digit hex value
+        result += QString("%1 ").arg(static_cast<quint8>(data.at(i)), 2, 16, QChar('0')).toUpper();
+    }
+
+    return result.trimmed();
+}
+
+void MainWindow::data_ready_hdlr()
+{
+    QString log_str;
+
+    while (udpSocket.hasPendingDatagrams())
+    {
+        QNetworkDatagram datagram = udpSocket.receiveDatagram();
+
+        recv_data_with_notes_s_t data_with_notes = {NORMAL, datagram.data()};
+        QByteArray &data = data_with_notes.data;
+
+        switch(collectingState)
+        {
+        case ST_IDLE:
+            if (data == m_start_req)
+            {
+                m_rmt_addr = datagram.senderAddress();
+                m_rmt_port = datagram.senderPort();
+
+                ui->rmtIPLEdit->setText(m_rmt_addr.toString());
+                ui->rmtPortLEdit->setText(QString::number(m_rmt_port));
+
+                udpSocket.writeDatagram(m_start_ack, m_rmt_addr, m_rmt_port);
+                collectingState = ST_COLLECTING;
+
+                log_str = "receive start cmd. acked. enter ST_COLLECTING";
+            }
+            else
+            {
+                log_str = "receive unkonwn data in ST_IDLE\n";
+                log_str += data.toHex() + "\n";
+            }
+            break;
+
+        case ST_COLLECTING:
+        default:
+            if (data == m_stop_req)
+            {
+                udpSocket.writeDatagram(m_stop_ack, m_rmt_addr, m_rmt_port);
+                collectingState = ST_IDLE;
+
+                log_str = "receive stop cmd. acked. enter ST_IDLE";
+            }
+            else
+            {
+                log_str = "receive unexpected data in ST_COLLECTIN\n";
+                log_str += data.toHex() + "\n";
+            }
+            log_str += data.toHex() + "\n";
+            break;
+        }
+        ui->infoDispEdit->append(log_disp_prepender_str() + log_str);
+    }
+}
+
+QString MainWindow::log_disp_prepender_str()
+{
+    return (common_tool_get_curr_date_str() + "," + common_tool_get_curr_time_str() + ",");
+}
+
+void MainWindow::on_randDataRBtn_toggled(bool checked)
+{
+    ui->ptPerRowSpinBox->setEnabled(checked);
+    ui->infinDataCheckBox->setEnabled(checked);
+
+    if(checked) ui->rowsToSendSpinBox->setEnabled(!(ui->infinDataCheckBox->isChecked()));
+}
+
+void MainWindow::on_fileDataRBtn_toggled(bool checked)
+{
+    ui->fileFpnLEdit->setEnabled(checked);
+    ui->selFileBtn->setEnabled(checked);
+    ui->fileDescLEdit->setEnabled(checked);
+}
+
+
+void MainWindow::on_infinDataCheckBox_clicked(bool checked)
+{
+    ui->rowsToSendSpinBox->setEnabled(!checked);
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,78 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QUdpSocket>
+#include <QTimer>
+
+#include "config_recorder/uiconfigrecorder.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+enum CollectState
+{
+    ST_IDLE,
+    ST_COLLECTING,
+};
+
+typedef enum
+{
+    RAND_DATA_BYTES,
+    DATA_FROM_TXT_FILE,
+    DATA_FROM_BIN_FILE,
+}data_source_type_e_t;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    UiConfigRecorder m_cfg_recorder;
+    qobj_ptr_set_t m_cfg_filter_in, m_cfg_filter_out;
+
+    // UDP socket
+    QUdpSocket udpSocket;
+    quint16 m_localport;
+
+    // Counter for appending to data
+    quint16 counter;
+
+    CollectState collectingState = ST_IDLE;
+
+    QHostAddress m_rmt_addr;
+    quint16 m_rmt_port;
+    QByteArray m_start_req, m_start_ack, m_stop_req, m_stop_ack;
+
+    QByteArray m_curr_row_data;
+
+    int m_send_int_ms;
+    QTimer m_send_timer;
+
+    data_source_type_e_t m_data_source_type;
+
+    // Helper methods
+    bool validateInputs();
+    QByteArray generateRandomData(int byteCount);
+    QString byteArrayToHexString(const QByteArray &data);
+    void setupUI();
+
+    QString log_disp_prepender_str();
+
+    void send_one_row();
+
+private slots:
+    void data_ready_hdlr();
+    void on_sendBtn_clicked();
+    void on_randDataRBtn_toggled(bool checked);
+    void on_fileDataRBtn_toggled(bool checked);
+    void on_infinDataCheckBox_clicked(bool checked);
+    void send_int_timer_hdlr();
+};
+#endif // MAINWINDOW_H
